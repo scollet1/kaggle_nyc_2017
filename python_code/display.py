a4a0585c548b07c97ac2ca93ff93dc75de9a600b
sam <origamillamaa@gmail.com>
2017-08-26 02:32:32 +0000

    this is it

--- python_code/display.py
+++ python_code/display.py
@@ -17,16 +17,9 @@ from matplotlib import pyplot
 import seaborn
 import pandas as pandas
 
-<<<<<<< HEAD
-def display(agents, raw_data):
-    agents = pandas.read_csv('../data_out.csv')
-    agents.head()
-    seaborn.lmplot(x="distance", y="time", data=agents, fit_reg=True).savefig('testing.png')
-=======
 def display():
 	agents = pandas.read_csv('../data_out.csv')
 	agents.head()
 	seaborn.lmplot(x="distance", y="duration", data=agents, fit_reg=True).savefig('../test.png')
 
 display()
->>>>>>> 5fa18a8c0697a45b550d7c1985449c487ae1f958
